b2cb4093a443fc5c81608d61fbc53169195b5590
YoongJwe <78844504+YoongJwe@users.noreply.github.com>
2022-08-18 16:44:38 +0900

    Delete unused files

--- deployment/deploy-paasta-monitoring.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bosh -e micro-bosh -n -d paasta-monitoring deploy paasta-monitoring.yml  \
-	-o use-public-network-aws.yml \
-	-l paasta-monitoring-vars.yml \
-	-l ../../common/common_vars.yml
